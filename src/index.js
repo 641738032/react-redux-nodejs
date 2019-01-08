f275b631ef0fcfa5e157fe583aec0e42c244e996
zhuxiaolin <641738032@qq.com>
2019-01-08 15:51:39 +0800

    react-router (BrowserRouter Route Link)

--- src/index.js
+++ src/index.js
@@ -7,17 +7,40 @@ import { createStore, applyMiddleware, compose } from "redux";
 import { counter, addGun, removeGun, addGunAsync } from "./index.redux.js";
 import thunk from 'redux-thunk';
 import { Provider } from "react-redux";
-
+import { BrowserRouter, Route, Link} from "react-router-dom"
 const store = createStore(counter,compose(
     applyMiddleware(thunk),
     window.devToolsExtension ? window.devToolsExtension() : ()=>{}
 ))
-
+function Erying () {
+    return (<h2>二营</h2>)
+}
+function QiBingLian () {
+    return (<h2>骑兵连</h2>)
+}
 ReactDOM.render(
     (
     // Provider 放在最外层 传入store
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">一营</Link>
+                    </li>
+                    <li>
+                        <Link to="/erying">二营</Link>
+                    </li>
+                    <li>
+                        <Link to="/qibinglian">骑兵连</Link>
+                    </li>
+                </ul>
+                <Route path="/"  exact component={App}/>
+                <Route path="/erying" component={Erying}/>
+                <Route path="/qibinglian" component={QiBingLian}/>
+            </div>
+        </BrowserRouter>
+        {/* <App /> */}
     </Provider>
     ), 
     document.getElementById('root')
